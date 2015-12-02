702ec6c0d74e45c08a5a45173526ffb3fd7c58d3
Thomas Simonnet <thomas.simonnet@milibris.com>
2015-12-02 18:33:20 +0100

    Frame data parser kickstart

---
+++ scripts/scraper.js
@@ -0,0 +1,19 @@
+var GoogleSpreadsheet = require('google-spreadsheet');
+
+const RASHID_SHEET_ID = '1vAojjiCPwt9NYFBGA1zwcsWQYAsV6A7jMqzZsY2bp40';
+const CREDENTIALS = require('../Framy-d9194bf37da0.json')
+
+var rashid_sheet = new GoogleSpreadsheet(RASHID_SHEET_ID);
+
+
+rashid_sheet.useServiceAccountAuth(CREDENTIALS, function(err){
+    rashid_sheet.getInfo(function(title, updated, author, name, email, worksheets){
+        console.log(1);
+        console.log(title, updated, author, name, email, worksheets);
+    });
+
+    rashid_sheet.getRows(1, {}, function(err, rows){console.log('ROWS 1');console.log(rows);});
+
+    rashid_sheet.getCells(1, {'min-row':3, 'max-row':3, 'return-empty': true}, function(err, rows){console.log('ROWS 2');console.log(rows);});
+});
+
